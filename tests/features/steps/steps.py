7bf28392eca7f96ae4098dbc7c64961f177feadd
cgiacomini <corrado.giacomini@vev.com>
2024-06-28 15:29:33 +0200

    fix(rename): rename folder steps to impl

--- tests/features/steps/steps.py
+++ tests/features/steps/steps.py
@@ -5,12 +5,12 @@ import os
 project_root = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
 sys.path.insert(0, project_root)
 
-from steps.boot_notification_request_steps import *
-from steps.boot_notification_response_steps import *
-from steps.heartbeat_request_steps import *
-from steps.heartbeat_response_steps import *
-from steps.open_connection_steps import *
-from steps.close_connection_steps import *
+from impl.boot_notification_request_steps import *
+from impl.boot_notification_response_steps import *
+from impl.heartbeat_request_steps import *
+from impl.heartbeat_response_steps import *
+from impl.open_connection_steps import *
+from impl.close_connection_steps import *
 
 logging.basicConfig(level=logging.DEBUG)
 
